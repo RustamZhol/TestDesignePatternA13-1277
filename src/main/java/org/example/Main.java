b9760e9ea4b6277630217c43977b95092e4d72a2
rbabari <r.babari@gmail.com>
2024-11-06 13:47:43 -0500

    ajout de java doc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+/**
+* Description de la classe
+*
+* @version 1.0
+* @author r babari
+*
+ */
 public class Main {
+
+    public static String information; /** Chaine de caractere contenant de l'information */
+    public static String information2; /** Chaine de caractere contenant de l'information */
+    /**
+     *
+     * @param args est un tableau de parametre dans le cas d'execution en ligne
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
+
+    /**
+     *
+     * @param a int à additionner
+     * @param b int à additionner
+     * @return le resultat de l'adition
+     */
+    public int add(int a, int b)
+    {
+
+        return a + b;
+    }
+
 }
