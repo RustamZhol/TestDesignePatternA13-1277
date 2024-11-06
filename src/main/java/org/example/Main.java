3b40dd608fae24277e250cedd37100960b38408b
RustamZhol <6291255@bdeb.qc.ca>
2024-11-06 13:59:36 -0500

    Ajout java doc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+/**
+*description de la classe
+*
+* @version 1.0
+* @author rus
+*
+ */
+
 public class Main {
+    public static String information; /** A string containing information */
+
+    /**
+     * This is the main method of the program. It prints "Hello world!" to the console.
+     *
+     * @param args command line arguments (unused in this program)
+     */
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
+
+/**
+ *
+ */
